ac8c136ab202691d12cc818461cde7e19547c135
赵鹏 <zhaopeng@zhaopengdemacbook-pro.local>
2018-06-30 16:39:22 +0800

    批注修改。

--- UIView简介/UIView简介/ViewController.m
+++ UIView简介/UIView简介/ViewController.m
@@ -18,7 +18,7 @@
 
 #pragma mark ————— 生命周期 —————
 /**
- 控制器的view在加载完毕的时候调用此方法
+ 控制器的view在加载完毕的时候会调用此方法。
  */
 - (void)viewDidLoad
 {
@@ -35,7 +35,10 @@
 #pragma mark ————— 点击按钮1 —————
 - (IBAction)clickButton
 {
-    //如果控件不指定特定frame的话，则表示使用默认的frame，默认frame的坐标是(0,0)
+    /**
+     如果控件不指定特定frame的话，则表示使用默认的frame；
+     默认的frame的原点坐标是(0,0)，同时每个控件也都有各自默认的宽和高。
+     */
     UISwitch *s = [[UISwitch alloc] init];
     [self.view addSubview:s];
     
@@ -43,10 +46,18 @@
     UIStepper *stepper = [[UIStepper alloc] init];
     [self.view addSubview:stepper];
     
-    //把新控件插入到具体的位置
+    /**
+     把新的控件插到具体的位置的时候用到下面的两个方法：
+     - (void)insertSubview:(UIView *)view belowSubview:(UIView *)siblingSubview;
+     - (void)insertSubview:(UIView *)view aboveSubview:(UIView *)siblingSubview;
+     
+     将子控件放到数组的最后面，显示在最上面
+     - (void)bringSubviewToFront:(UIView *)view;
+     将子控件放到数组的最前面，显示在最下面
+     - (void)sendSubviewToBack:(UIView *)view;
+     */
     UISlider *slider = [[UISlider alloc] init];
     [self.view insertSubview:slider aboveSubview:stepper];
-//    [self.view insertSubview:slider belowSubview:stepper];
     
     //把控件从父控件上移除
     [self.label removeFromSuperview];
@@ -58,15 +69,17 @@
 #pragma mark ————— 点击按钮2 —————
 - (IBAction)clickButton2
 {
-    //UIView不像控件那样，没有默认的frame
+    /**
+     如果在创建UIView的实例对象的时候不设定frame的话则在视图上不会出现这个实例对象，因为UIView不像其他控件那样，其他控件有默认的frame，但是UIView的实例对象没有默认的frame，需要手动设定frame。
+     */
     UIView *view = [[UIView alloc] init];
     view.backgroundColor = [UIColor redColor];
     view.frame = CGRectMake(0, 0, 100, 100);
     [self.view addSubview:view];
     
     /**
-     控件的中心点在父视图中的坐标；
-     利用NSStringFromCGPoint方法，把结构体转化成字符串。
+     center代表这个控件的中心点在它父视图中的位置；
+     利用NSStringFromCGPoint方法，可以把结构体转化成字符串。
      */
     NSLog(@"%@", NSStringFromCGPoint(view.center));
     
